15351b1e5a0e9ac7658e29187148fed344d57498
Karthik Mahadevan <2187870+Kar-Ma@users.noreply.github.com>
2026-07-21 12:06:24 +0200

    Initial iOS project

---
+++ Vigil/VigilApp.swift
@@ -0,0 +1,17 @@
+//
+//  VigilApp.swift
+//  Vigil
+//
+//  Created by Karthik Mahadevan on 21/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct VigilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VigilTests/VigilTests.swift
@@ -0,0 +1,19 @@
+//
+//  VigilTests.swift
+//  VigilTests
+//
+//  Created by Karthik Mahadevan on 21/07/2026.
+//
+
+import Testing
+@testable import Vigil
+
+struct VigilTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
